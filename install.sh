288b24e9de83d49aefd1549a6e27d72e6ab25ee6
ithewei <ithewei@163.com>
2019-01-07 18:29:17 +0800

    update

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-cat <<EOF > ~/.vimrc
-set rtp+=~/.vim/scripts
-source ~/.vim/scripts/basic.vim
-source ~/.vim/scripts/plugin.vim
+if [ -e ~/.vimrc ]; then
+    mv ~/.vimrc ~/.vimrc.bak
+fi
 
-EOF
+echo "install succeed."
---
+++ vimrc
@@ -0,0 +1,4 @@
+set rtp+=~/.vim/scripts
+source ~/.vim/scripts/basic.vim
+source ~/.vim/scripts/plugin.vim
+
