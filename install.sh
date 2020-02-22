7c639761291c122407fb768bbb1f06cb51d7f094
ithewei <ithewei@163.com>
2020-02-22 13:20:06 +0800

    add .tmux.conf

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://gitee.com/ithewei/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 if [ -e ~/.vimrc ]; then
     mv ~/.vimrc ~/.vimrc.bak
 fi
+
+if [ -e ~/.tmux.conf ] && [ ! -e ~/.tmux.conf.bak ]; then
+    mv ~/.tmux.conf ~/.tmux.conf.bak
+fi
+cp ~/.vim/.tmux.conf ~/.tmux.conf
 
 echo "install succeed."
